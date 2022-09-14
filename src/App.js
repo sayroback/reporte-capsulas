4ff06e1d8395873e32771971174bb0794dd393b9
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-09-14 13:40:15 -0500

    reporte de las capsulas para imprimir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  function printpage() {
+    // let newstr = document.getElementById("app").innerHTML;
+    // let oldstr = document.body.innerHTML;
+    // document.body.innerHTML = newstr;
+    // document.body.innerHTML = oldstr;
+    window.print();
+    return false;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="App">
+      <div id="impresion" className="reporte">
+        <h1>Información de la muestra tomada</h1>
+        <div className="reporte_header">
+          <div>
+            <p>
+              <b>Núm. de lote: </b>
+            </p>
+            <p>XX1234</p>
+          </div>
+          <div>
+            <p>
+              <b>Fecha y hora de la muestra: </b>
+            </p>
+            <p>23/09/2021</p>
+          </div>
+          <div>
+            <p>
+              <b>Usuario que tomó la muestra: </b>
+            </p>
+            <p>XXXXXXX</p>
+          </div>
+        </div>
+        <div className="reporte_imagenes">
+          <p>
+            <b>Imagen de la muestra:</b>
+          </p>
+          <div>
+            <img
+              width="200"
+              src="https://i.ibb.co/BHhBQq1/foto-capsula.png"
+              alt="foto-capsula"
+            />
+          </div>
+          <p>
+            <b>Dimensiones de los espesores mínimo y máximo: </b>
+          </p>
+          <div className="reporte_imagenes_diametros">
+            <div>
+              <div>
+                <img
+                  width="200"
+                  src="https://i.ibb.co/wyVRMqm/diametro-interior-capsula.png"
+                  alt="diametro-interior-capsula"
+                />
+              </div>
+              <span>
+                <b>mínimo = </b>
+              </span>
+              <span>123px</span>
+            </div>
+            <div>
+              <div>
+                <img
+                  width="200"
+                  src="https://i.ibb.co/dQMhvr4/diametro-exterior-capsula.png.png"
+                  alt="diametro-exterior-capsula"
+                />
+              </div>
+              <span>
+                <b>máximo = </b>
+              </span>
+              <span>234px</span>
+            </div>
+          </div>
+        </div>
+        <div className="reporte_resultado">
+          <div>
+            <p>
+              <b>Razón entre el espesor mínimo y máximo: </b>
+            </p>
+            <p>r = 123</p>
+          </div>
+          <div>
+            <p>
+              <b>
+                Diferencia porcentual de r con respecto a los criterios
+                establecidos como estándar:{" "}
+              </b>
+            </p>
+            <p>Δr = 12%</p>
+          </div>
+        </div>
+        <div className="reporte_graficas">
+          <div>
+            <p>
+              <b>Gráfica de control sobre las muestras</b>
+            </p>
+            <img
+              src="https://i.ibb.co/42CrL5q/grafica-limites-inferior-superior.png"
+              alt="grafica-limites-inferior-superior"
+            />
+          </div>
+          <div>
+            <p>
+              <b>
+                Histograma donde se observe la cantidad de muestras que cayeron
+                dentro de un rango de medida especifico y la desviación que
+                presentan respecto al valor establecido como criterio correcto
+              </b>
+            </p>
+            <img
+              src="https://i.ibb.co/pPWph8F/histograma.png"
+              alt="grafica-limites-inferior-superior"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="imprimir">
+        <button onClick={() => printpage()}>Imprimir reporte</button>
+      </div>
     </div>
   );
 }
